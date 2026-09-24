a7ed1645805518c27061b3b953da2653e960e8d4
Ro-chie Mirabueno <rochie.lumor@gmail.com>
2026-09-24 10:20:22 +0800

    Add files via upload

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -5,6 +5,9 @@
         header("Location: ../index.php");
         exit;
     }
+    $students = mysqli_query($conn,"SELECT id FROM users WHERE role = 'student'");
+    $subjects = mysqli_query($conn,"SELECT id FROM subjects ");
+    $enrollments = mysqli_query($conn,"SELECT id FROM enrollments");
 ?>
 
 
@@ -60,7 +63,7 @@
         <h2>Admin Dashboard</h2>
 
         <p class="text-muted">
-            Welcome, System Administrator.
+            Welcome, <?php echo htmlspecialchars($_SESSION["full_name"])?>;
         </p>
 
         <div class="row g-3">
@@ -72,7 +75,7 @@
 
                         <h6>Student Accounts</h6>
 
-                        <h2>10</h2>
+                        <h2><?php echo mysqli_num_rows($students);?></h2>
 
                         <a
                             href="students.html"
@@ -92,7 +95,7 @@
 
                         <h6>Subjects</h6>
 
-                        <h2>8</h2>
+                        <h2><?php echo mysqli_num_rows($subjects);?></h2>
 
                         <a
                             href="subjects.html"
@@ -112,7 +115,7 @@
 
                         <h6>Enrollments</h6>
 
-                        <h2>24</h2>
+                        <h2><?php echo mysqli_num_rows($enrollments);?></h2>
 
                         <span class="text-muted small">
                             Managed from Student Records
